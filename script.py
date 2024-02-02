4d6c436eca57cbed4e0f4a0893d04205ca4c48b7
dxia2p <d.xia111@gmail.com>
2024-02-02 09:22:47 -0700

    a

--- script.py
+++ script.py
@@ -1,9 +1,9 @@
 import math
 
-def dist(x1, y1, x2, y2):
+def dist(x1, y1, x2, y2): # DISTANCE FUNCTION TO CALCULTE DISTANCE
     return math.sqrt((y2 - y1)**2 + (x2 - x1)**2)
 
-print("WELCOME TO THE TRIANGLE PERIMETER PROGRAM")
+print("WELCOME TO THE TRIANGLE PERIMETER PROGRAM") # GET INPUT
 print("Enter the coordinates of the vertices of a triangle...")
 print("\n")
 
@@ -22,11 +22,11 @@ x3 = int(input("Enter x-coordinate for Vertex C: "))
 y3 = int(input("Enter y-coordinate for Vertex C: "))
 print("\n")
 
-Ab = dist(x1, y1, x2, y2) 
+Ab = dist(x1, y1, x2, y2) # CALCULATE DISTANCES
 Ac = dist(x3, y3, x1, y1)
 Bc = dist(x2, y2, x3, y3)
 
-print("SIDE LENGTHS & PERIMETER:")
+print("SIDE LENGTHS & PERIMETER:") # OUTPUT DISTANCES TO USER
 print("AB = " + str(Ab))
 print("AC = " + str(Ac))
 print("BC = " + str(Bc))
